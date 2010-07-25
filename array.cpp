a75b161b5f9724568590e43dbfe1e2ed5836abca
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 16:27:56 +0800

    n

--- array.cpp
+++ array.cpp
@@ -5,10 +5,10 @@ int main(void)
 {
 	int	a[2][3]={1,2,3,4,5,6}; 
 	int	*b=a[0]; 
-	cout < <b[0] < <endl; 
-	cout < <b[1] < <endl; 
+	cout <<b[0] <<endl; 
+	cout <<b[1] <<endl; 
 	b=a[1]; 
-	cout < <b[0] < <endl; 
-	cout < <b[1] < <endl; 
+	cout <<b[0] <<endl; 
+	cout <<b[1] <<endl; 
 	return 0;
 }
