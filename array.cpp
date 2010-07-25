61bae499cc6f9e5d95c93b18914ae82f0a32a80b
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 16:31:05 +0800

    n

--- array.cpp
+++ array.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <iostream>
 using namespace std;
 
@@ -10,5 +11,21 @@ int main(void)
 	b=a[1]; 
 	cout <<b[0] <<endl; 
 	cout <<b[1] <<endl; 
+	
+	int   a1[2][3]={6,5,4,3,2,1}; 
+        printf( "%d   :   %d\n ",   a1   ,sizeof(   a1   ));                                         //a1 
+        printf( "%d   :   %d\n ",   *a1   ,sizeof(   *a1   ));                                     //*a1 
+        printf( "%d   :   %d\n ",   a1[0]   ,sizeof(   a1[0]   ));                             //*a1 
+        printf( "%d   :   %d\n ",   *(a1+1)   ,sizeof(   *(a1+1)   ));                     //*(a1+1) 
+        printf( "%d   :   %d\n ",   a1[1]   ,sizeof(   a1[1]   ));                             //*(a1+1) 
+        printf( "%d   :   %d\n ",   **a1   ,sizeof(   **a1   ));                                 //a1[0][0]=6 
+        printf( "%d   :   %d\n ",   *a1[0]   ,sizeof(   *a1[0]   ));                         //a1[0][0]=6 
+        printf( "%d   :   %d\n ",   *(*(a1+1)+1)   ,sizeof(   *(*(a1+1)+1)   ));//a1[1][1]=2 
+        printf( "%d   :   %d\n ",   *(*a1+1)   ,sizeof(   *(*a1+1)   ));                 //a1[0][1]=5 
+        printf( "%d   :   %d\n ",   **(a1+1)   ,sizeof(   **(a1+1)   ));                 //a1[1][0]=3 
+        printf( "%d   :   %d\n ",   *a1[1]   ,sizeof(   *a1[1]   ));                         //a1[1][0]=3 
+        printf( "%d   :   %d\n ",   &a1   ,sizeof(   &a1   ));                                     //&a1[0][0] 
+        printf( "%d   :   %d\n ",   &a1[0]   ,sizeof(   &a1[0]   ));                         //&a1[0][0]
+        
 	return 0;
 }
