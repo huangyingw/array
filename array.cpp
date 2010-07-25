ec42dfb107052ea82dd91d7cdd76d682a596d89a
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 16:27:27 +0800

    n

--- array.cpp
+++ array.cpp
@@ -1,14 +1,14 @@
-typedef char* pStr;
-//#define pStr char* ;
+#include <iostream>
+using namespace std;
+
 int main(void)
 {
-	const int n=5;
-	//int a[n];
-	
-	char string[4]="abc";
-	const char *p1 = string;
-	const pStr p2 = string;
-	p1++;
-	p2++;
+	int	a[2][3]={1,2,3,4,5,6}; 
+	int	*b=a[0]; 
+	cout < <b[0] < <endl; 
+	cout < <b[1] < <endl; 
+	b=a[1]; 
+	cout < <b[0] < <endl; 
+	cout < <b[1] < <endl; 
 	return 0;
 }
