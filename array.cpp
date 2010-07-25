e4e8d802f8e64ac70acd2ff4dde645dc612e3cf5
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 16:32:40 +0800

    n

--- array.cpp
+++ array.cpp
@@ -13,19 +13,19 @@ int main(void)
 	cout <<b[1] <<endl; 
 	
 	int   a1[2][3]={6,5,4,3,2,1}; 
-        printf( "%d   :   %d\n ",   a1   ,sizeof(   a1   ));                                         //a1 
-        printf( "%d   :   %d\n ",   *a1   ,sizeof(   *a1   ));                                     //*a1 
-        printf( "%d   :   %d\n ",   a1[0]   ,sizeof(   a1[0]   ));                             //*a1 
-        printf( "%d   :   %d\n ",   *(a1+1)   ,sizeof(   *(a1+1)   ));                     //*(a1+1) 
-        printf( "%d   :   %d\n ",   a1[1]   ,sizeof(   a1[1]   ));                             //*(a1+1) 
-        printf( "%d   :   %d\n ",   **a1   ,sizeof(   **a1   ));                                 //a1[0][0]=6 
-        printf( "%d   :   %d\n ",   *a1[0]   ,sizeof(   *a1[0]   ));                         //a1[0][0]=6 
-        printf( "%d   :   %d\n ",   *(*(a1+1)+1)   ,sizeof(   *(*(a1+1)+1)   ));//a1[1][1]=2 
-        printf( "%d   :   %d\n ",   *(*a1+1)   ,sizeof(   *(*a1+1)   ));                 //a1[0][1]=5 
-        printf( "%d   :   %d\n ",   **(a1+1)   ,sizeof(   **(a1+1)   ));                 //a1[1][0]=3 
-        printf( "%d   :   %d\n ",   *a1[1]   ,sizeof(   *a1[1]   ));                         //a1[1][0]=3 
-        printf( "%d   :   %d\n ",   &a1   ,sizeof(   &a1   ));                                     //&a1[0][0] 
-        printf( "%d   :   %d\n ",   &a1[0]   ,sizeof(   &a1[0]   ));                         //&a1[0][0]
-        
+  printf( "%d   :   %d\n ",   a1,sizeof(a1));//a1 
+  printf( "%d   :   %d\n ",   *a1,sizeof(*a1));//*a1 
+  printf( "%d   :   %d\n ",   a1[0],sizeof(a1[0]));//*a1 
+  printf( "%d   :   %d\n ",   *(a1+1),sizeof(*(a1+1)));//*(a1+1) 
+  printf( "%d   :   %d\n ",   a1[1]   ,sizeof(a1[1]));//*(a1+1) 
+  printf( "%d   :   %d\n ",   **a1   ,sizeof(**a1));//a1[0][0]=6 
+  printf( "%d   :   %d\n ",   *a1[0]   ,sizeof(*a1[0]));//a1[0][0]=6 
+  printf( "%d   :   %d\n ",   *(*(a1+1)+1)   ,sizeof(*(*(a1+1)+1)));//a1[1][1]=2 
+  printf( "%d   :   %d\n ",   *(*a1+1)   ,sizeof(*(*a1+1)));//a1[0][1]=5 
+  printf( "%d   :   %d\n ",   **(a1+1)   ,sizeof(**(a1+1)));//a1[1][0]=3 
+  printf( "%d   :   %d\n ",   *a1[1]   ,sizeof(*a1[1]));//a1[1][0]=3 
+  printf( "%d   :   %d\n ",   &a1   ,sizeof(&a1));//&a1[0][0] 
+  printf( "%d   :   %d\n ",   &a1[0]   ,sizeof(&a1[0]));//&a1[0][0]
+  
 	return 0;
 }
