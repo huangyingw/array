6dd3dcabcad81e81454c59418ff424620de1900c
huangyingw@linux_programming <huangyingw@gmail.com>
2010-07-25 16:33:17 +0800

    n

--- array.cpp
+++ array.cpp
@@ -12,20 +12,20 @@ int main(void)
 	cout <<b[0] <<endl; 
 	cout <<b[1] <<endl; 
 	
-	int   a1[2][3]={6,5,4,3,2,1}; 
-  printf( "%d   :   %d\n ",   a1,sizeof(a1));//a1 
-  printf( "%d   :   %d\n ",   *a1,sizeof(*a1));//*a1 
-  printf( "%d   :   %d\n ",   a1[0],sizeof(a1[0]));//*a1 
-  printf( "%d   :   %d\n ",   *(a1+1),sizeof(*(a1+1)));//*(a1+1) 
-  printf( "%d   :   %d\n ",   a1[1]   ,sizeof(a1[1]));//*(a1+1) 
-  printf( "%d   :   %d\n ",   **a1   ,sizeof(**a1));//a1[0][0]=6 
-  printf( "%d   :   %d\n ",   *a1[0]   ,sizeof(*a1[0]));//a1[0][0]=6 
-  printf( "%d   :   %d\n ",   *(*(a1+1)+1)   ,sizeof(*(*(a1+1)+1)));//a1[1][1]=2 
-  printf( "%d   :   %d\n ",   *(*a1+1)   ,sizeof(*(*a1+1)));//a1[0][1]=5 
-  printf( "%d   :   %d\n ",   **(a1+1)   ,sizeof(**(a1+1)));//a1[1][0]=3 
-  printf( "%d   :   %d\n ",   *a1[1]   ,sizeof(*a1[1]));//a1[1][0]=3 
-  printf( "%d   :   %d\n ",   &a1   ,sizeof(&a1));//&a1[0][0] 
-  printf( "%d   :   %d\n ",   &a1[0]   ,sizeof(&a1[0]));//&a1[0][0]
+	int	a1[2][3]={6,5,4,3,2,1}; 
+  printf( "%d   :   %d\n ",a1,sizeof(a1));//a1 
+  printf( "%d   :   %d\n ",*a1,sizeof(*a1));//*a1 
+  printf( "%d   :   %d\n ",a1[0],sizeof(a1[0]));//*a1 
+  printf( "%d   :   %d\n ",*(a1+1),sizeof(*(a1+1)));//*(a1+1) 
+  printf( "%d   :   %d\n ",a1[1],sizeof(a1[1]));//*(a1+1) 
+  printf( "%d   :   %d\n ",**a1,sizeof(**a1));//a1[0][0]=6 
+  printf( "%d   :   %d\n ",*a1[0],sizeof(*a1[0]));//a1[0][0]=6 
+  printf( "%d   :   %d\n ",*(*(a1+1)+1),sizeof(*(*(a1+1)+1)));//a1[1][1]=2 
+  printf( "%d   :   %d\n ",*(*a1+1),sizeof(*(*a1+1)));//a1[0][1]=5 
+  printf( "%d   :   %d\n ",**(a1+1),sizeof(**(a1+1)));//a1[1][0]=3 
+  printf( "%d   :   %d\n ",*a1[1],sizeof(*a1[1]));//a1[1][0]=3 
+  printf( "%d   :   %d\n ",&a1,sizeof(&a1));//&a1[0][0] 
+  printf( "%d   :   %d\n ",&a1[0],sizeof(&a1[0]));//&a1[0][0]
   
 	return 0;
 }
