24d0ecba208fd86b7489a969d654b8254a1a8625
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-05 22:07:48 +0800

    n

--- array.cc
+++ array.cc
@@ -2,6 +2,38 @@
 #include <iostream>
 using namespace std;
 
+void array_copy(int array[], int len) 
+{
+  //int result[]=new int[len];
+  //int *result=new int[len];
+  int result[len];
+  for (int i=0;i<len;i++)
+  {
+    result[i]=array[i];
+  }
+  int temp;
+  temp=result[0];
+  result[0]=result[1];
+  result[1]=temp;
+}
+
+void array_uncopy(int array[], int len) 
+{
+  int temp;
+  temp=array[0];
+  array[0]=array[1];
+  array[1]=temp;
+}
+
+void print_array(int array[], int len)
+{
+  for (int i=0;i<len;i++)
+  {
+    cout<<array[i]<<",";
+  }
+  cout<<endl;
+}
+
 int main(void)
 {
   int	a1[2][3]={6,5,4,3,2,1};
@@ -19,5 +51,18 @@ int main(void)
   printf( "%d   :   %d\n ",&a1,sizeof(&a1));//&a1[0][0]	-1075833576   :   4
   printf( "%d   :   %d\n ",&a1[0],sizeof(&a1[0]));//&a1[0][0]	-1075833576   :   4
 
+  int a[] = {4, 2, 5};
+
+  print_array(a,sizeof(a)/sizeof(int));
+  array_copy(a,sizeof(a)/sizeof(int));
+  print_array(a,sizeof(a)/sizeof(int));
+
+  cout<<endl;
+  print_array(a,sizeof(a)/sizeof(int));
+  array_uncopy(a,sizeof(a)/sizeof(int));
+  print_array(a,sizeof(a)/sizeof(int));
+  //
+  //int b[]=new int []{1,2,3};
+  //array_uncopy((new int[]{1,2,3}),3);
   return 0;
 }
