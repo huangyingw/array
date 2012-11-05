25a560484c585a124e1bd1bc686dda15b1602f48
huangyingw@linux_programming <huangyingw@gmail.com>
2012-11-04 22:31:41 -0800

    n

--- array.cc
+++ array.cc
@@ -38,7 +38,6 @@ void print_array2(int array[][4], int len1,int len2) {
   cout<<endl;
 }
 
-
 int main(void) {
   int	a1[2][3]={6,5,4,3,2,1};
   printf( "%d   :   %d\n ",a1,sizeof(a1));//a1	-1075833576   :   24
@@ -52,8 +51,8 @@ int main(void) {
   printf( "%d   :   %d\n ",*(*a1+1),sizeof(*(*a1+1)));//a1[0][1]=5	5   :   4
   printf( "%d   :   %d\n ",**(a1+1),sizeof(**(a1+1)));//a1[1][0]=3	3   :   4
   printf( "%d   :   %d\n ",*a1[1],sizeof(*a1[1]));//a1[1][0]=3	3   :   4
-  printf( "%d   :   %d\n ",&a1,sizeof(&a1));//&a1[0][0]	-1075833576   :   4
-  printf( "%d   :   %d\n ",&a1[0],sizeof(&a1[0]));//&a1[0][0]	-1075833576   :   4
+  printf( "%d   :   %d\n ",&a1,sizeof(&a1));//&a1[0][0]	-1075833576   :   8
+  printf( "%d   :   %d\n ",&a1[0],sizeof(&a1[0]));//&a1[0][0]	-1075833576   :   8
 
   //a1[0]={1,2,3};
 
